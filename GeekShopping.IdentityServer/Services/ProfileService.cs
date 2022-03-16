d65e1e626387bc5498bcd094d29f939c25ad5677
Bruno <pj_antoniobruno@bocombbm.com.br>
2022-03-15 21:01:03 -0300

    Seção 11 - Autenticação com Oauth, OpenId, JWT - 15032100

--- GeekShopping.IdentityServer/Services/ProfileService.cs
+++ GeekShopping.IdentityServer/Services/ProfileService.cs
@@ -4,11 +4,7 @@ using Duende.IdentityServer.Services;
 using GeekShopping.IdentityServer.Model;
 using IdentityModel;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace GeekShopping.IdentityServer.Services
 {
